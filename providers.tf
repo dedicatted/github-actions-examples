ef32293dd7d3eddef444dc78f2ef4a44e5d5f924
ypryimak@dedicatted.com <ypryimak@dedicatted.com>
2024-02-08 13:42:59 +0200

    fix

--- providers.tf
+++ providers.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "your-terraform-state-bucket-name"
     key            = "terraform/state.tfstate" 
-    region         = var.region   
+    region         = "us-west-2"
     dynamodb_table = "terraform-lock-table"
     encrypt        = true
   }
